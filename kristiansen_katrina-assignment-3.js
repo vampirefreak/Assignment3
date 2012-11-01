3912a008e8087d4f329950ebac2609cb45b5e45f
katrina kristiansen <k.swim1983@gmail.com>
2012-11-01 16:35:17 -0500

    commit 1
    commit 1 assignment 3

---
+++ kristiansen_katrina-assignment-3.js
@@ -0,0 +1,64 @@
+// Katrina Kristiansen
+// SDI 1
+// 1210
+
+// Global Variables
+
+var relax = " Write Vampire and the Alchemist. ";
+function activity(){
+    
+}
+activity();
+console.log(relax);
+
+// conditional
+var a = 1;
+if (a === 1){
+    console.log("Channing gave his life for equilvlent exchange to make Kitty human.");
+    a = 0
+}
+else {
+    console.log("Channing did not make the right elixer and died for nothing." + a)
+};
+
+var channingHeart = 10;
+var channingElix =  234;
+var channingGiveUp = 0;
+if(channingHeart == 10 || channingHeart == 20)
+{
+    if(channingElix >= 231 && channingElix <= 400)
+        channingGiveUp = "everything";
+        else
+        channingGiveUp = "just his heart";
+        console.log("Channing is giving up " + channingHeart + " percent of his being. " );
+}
+else if(channingHeart == 15 || channingHeart == 36)
+
+      if (channingElix >= 450 && channingElix <= 500)
+           channingGiveUp = " His sight.";
+       else
+          channingGiveUp = " His hearing. "
+
+       else
+          console.log("channing gave up everything to show kitty his love " + channingHeart);
+          
+// global variable
+
+var a = 10;
+
+alchemistJason();
+
+function alchemistJason()
+   {
+   var a = 20;
+   console.log(" Alchemist Jason is saddend that Channing got the right formula" );
+   }
+
+console.log("Jason makes elixer " + a + " memory for Kitty to remember Channing.");
+
+// return a value
+ function jasonHelp(a,b){
+    return a*b;
+ }
+console.log("Jason puts " + jasonHelp(50*40) + " percent into making Kitty happy again.")
+
